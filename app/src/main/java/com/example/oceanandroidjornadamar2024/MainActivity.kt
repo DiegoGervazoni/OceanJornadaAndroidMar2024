e296834fa8c4c017005ee8ad86eb12a285669b4a
DiegoGervazoni <diegoo.gervazoni@hotmail.com>
2024-03-05 20:15:06 -0300

    Comportamento do TextView no Button

--- app/src/main/java/com/example/oceanandroidjornadamar2024/MainActivity.kt
+++ app/src/main/java/com/example/oceanandroidjornadamar2024/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.oceanandroidjornadamar2024
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Procura pelo ID com o findViewer
+        val btnEnviar = findViewById<Button>(R.id.btnEnviar)
+        val tvResultado = findViewById<TextView>(R.id.tvResultado)
+
+        // Para criar um Listener
+        btnEnviar.setOnClickListener{
+            tvResultado.text = "E lá vamos nós!"
+        }
     }
 }
