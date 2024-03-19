25a3f70fe23023da1d5b94a97ed2658845443104
DiegoGervazoni <diegoo.gervazoni@hotmail.com>
2024-03-19 19:03:56 -0300

    Criacao do ItemAdapter (WIP)

---
+++ app/src/main/java/com/example/oceanandroidjornadamar2024/ItemAdapter.kt
@@ -0,0 +1,35 @@
+package com.example.oceanandroidjornadamar2024
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class ItemAdapter(val itens: List<Item>) : RecyclerView.Adapter<ItemAdapter.ViewHolder>() {
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bindView(item: Item) {
+            val textView = itemView.findViewById<TextView>(R.id.textView)
+            val imageView = itemView.findViewById<ImageView>(R.id.imageView)
+
+            textView.text = item.nome
+            // TODO: implementar carregamento da imagem com Glide
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_layout, parent, false)
+        return ViewHolder(itemView)
+    }
+
+    override fun getItemCount(): Int {
+        return itens.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = itens[position]
+        holder.bindView(item)
+    }
+}
